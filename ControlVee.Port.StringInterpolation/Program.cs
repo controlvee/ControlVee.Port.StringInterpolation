cc8e5284d2d218a124631ca9a8b95f8bf06c55d2
controlvee <adam.goerlich@gmail.com>
2021-02-01 14:23:53 -0600

    Add project files.

---
+++ ControlVee.Port.StringInterpolation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ControlVee.Port.StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string sp = " ";
+            string world = "World";
+            string exp = "!";
+
+            Console.WriteLine("Hello World!");
+
+            //
+            // OR
+            //
+
+            Console.WriteLine(hello + sp + world + exp);
+
+            //
+            // OR
+            //
+
+            Console.WriteLine("{0} {1}!", hello, world);
+
+            //
+            // OR 
+            //
+
+            Console.WriteLine($"{hello} {world}!");
+            Console.Read();
+        }
+    }
+}
